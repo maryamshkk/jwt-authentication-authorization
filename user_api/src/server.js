232c9f01f43feeff5d9411612cef2f8aecdbb82a
maryamshkk <maryamshk117@gmail.com>
2026-08-17 17:03:25 +0500

    user_api

---
+++ user_api/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js"
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/",(req, res) => {
+    res.json({
+        message: "server is running"
+    });
+})
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+})
